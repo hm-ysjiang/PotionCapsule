fc0770953255a1d85642244db817dda89bcdd4e0
hm-ysjiang <hmysjiang.git@gmail.com>
2020-01-17 03:17:47 +0800

    Add hover tooltip for buttons

--- src/main/java/hmysjiang/potioncapsule/client/gui/ScreenPendant.java
+++ src/main/java/hmysjiang/potioncapsule/client/gui/ScreenPendant.java
@@ -13,6 +13,7 @@ import hmysjiang.potioncapsule.network.PacketHandler;
 import hmysjiang.potioncapsule.network.packets.CPacketUpdatePendantStatus;
 import hmysjiang.potioncapsule.utils.Defaults;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -39,56 +40,72 @@ public class ScreenPendant extends ContainerScreen<ContainerPendant> {
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 0));
-				}, mask & (1 << 0)));
+				}, mask & (1 << 0), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 35, guiTop + 33, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 1));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 1));
-				}, mask & (1 << 1)));
+				}, mask & (1 << 1), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 115, guiTop + 33, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 2));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 2));
-				}, mask & (1 << 2)));
+				}, mask & (1 << 2), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 15, guiTop + 68, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 3));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 3));
-				}, mask & (1 << 3)));
+				}, mask & (1 << 3), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 135, guiTop + 68, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 4));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 4));
-				}, mask & (1 << 4)));
+				}, mask & (1 << 4), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 35, guiTop + 103, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 5));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 5));
-				}, mask & (1 << 5)));
+				}, mask & (1 << 5), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 115, guiTop + 103, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 6));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 6));
-				}, mask & (1 << 6)));
+				}, mask & (1 << 6), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 75, guiTop + 119, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 7));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 7));
-				}, mask & (1 << 7)));
+				}, mask & (1 << 7), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 
 		addButton(new PairedImageButtonSwitch(guiLeft + 115, guiTop + 138, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
@@ -96,21 +113,27 @@ public class ScreenPendant extends ContainerScreen<ContainerPendant> {
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 8));
-				}, mask & (1 << 8)));
+				}, mask & (1 << 8), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 133, guiTop + 138, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 9));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 9));
-				}, mask & (1 << 9)));
+				}, mask & (1 << 9), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 		addButton(new PairedImageButtonSwitch(guiLeft + 151, guiTop + 138, 4, 4, 180, 0, 176, 0, 5, TEXTURE, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 10));
 				}, 
 				btn -> {
 					PacketHandler.getInstacne().sendToServer(new CPacketUpdatePendantStatus(1 << 10));
-				}, mask & (1 << 10)));
+				}, mask & (1 << 10), 
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_off").applyTextStyle(TextFormatting.RED)),
+				new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status").appendSibling(new TranslationTextComponent("potioncapsule.gui.pendant.tooltip.status_on").applyTextStyle(TextFormatting.GREEN))));
 	}
 	
 	@Override
@@ -157,6 +180,13 @@ public class ScreenPendant extends ContainerScreen<ContainerPendant> {
 			else
 				super.renderHoveredToolTip(mouseX, mouseY);
 		}
+		for (Widget wg: buttons)
+			if (wg.isHovered()){
+				ITextComponent text = ((PairedImageButtonSwitch) wg).getTooltip();
+				if (text != null) {
+					this.renderTooltip(text.getFormattedText(), mouseX, mouseY);
+				}
+			}
 	}
 	
 }
--- src/main/java/hmysjiang/potioncapsule/client/gui/widgets/PairedImageButtonSwitch.java
+++ src/main/java/hmysjiang/potioncapsule/client/gui/widgets/PairedImageButtonSwitch.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.button.ImageButton;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -15,6 +16,7 @@ public class PairedImageButtonSwitch extends ImageButton {
 	private final ResourceLocation TEXTURE;
 	private final IPressable action_F, action_T;
 	private boolean flag;
+	private ITextComponent hover_F, hover_T;
 	
 	public PairedImageButtonSwitch(int xIn, int yIn, int widthIn, int heightIn, int imgPosX_F, int imgPosY_F, int imgPosX_T, int imgPosY_T, int yDiff, ResourceLocation texture, IPressable action_F, IPressable action_T) {
 		this(xIn, yIn, widthIn, heightIn, imgPosX_F, imgPosY_F, imgPosX_T, imgPosY_T, yDiff, texture, action_F, action_T, false);
@@ -24,7 +26,15 @@ public class PairedImageButtonSwitch extends ImageButton {
 		this(xIn, yIn, widthIn, heightIn, imgPosX_F, imgPosY_F, imgPosX_T, imgPosY_T, yDiff, texture, action_F, action_T, flagmask > 0);
 	}
 	
+	public PairedImageButtonSwitch(int xIn, int yIn, int widthIn, int heightIn, int imgPosX_F, int imgPosY_F, int imgPosX_T, int imgPosY_T, int yDiff, ResourceLocation texture, IPressable action_F, IPressable action_T, int flagmask, ITextComponent hoverText_F, ITextComponent hoverText_T) {
+		this(xIn, yIn, widthIn, heightIn, imgPosX_F, imgPosY_F, imgPosX_T, imgPosY_T, yDiff, texture, action_F, action_T, flagmask > 0, hoverText_F, hoverText_T);
+	}
+	
 	public PairedImageButtonSwitch(int xIn, int yIn, int widthIn, int heightIn, int imgPosX_F, int imgPosY_F, int imgPosX_T, int imgPosY_T, int yDiff, ResourceLocation texture, IPressable action_F, IPressable action_T, boolean flag) {
+		this(xIn, yIn, widthIn, heightIn, imgPosX_F, imgPosY_F, imgPosX_T, imgPosY_T, yDiff, texture, action_F, action_T, flag, null, null);
+	}
+	
+	public PairedImageButtonSwitch(int xIn, int yIn, int widthIn, int heightIn, int imgPosX_F, int imgPosY_F, int imgPosX_T, int imgPosY_T, int yDiff, ResourceLocation texture, IPressable action_F, IPressable action_T, boolean flag, ITextComponent hoverText_F, ITextComponent hoverText_T) {
 		super(xIn, yIn, widthIn, heightIn, imgPosX_F, imgPosY_F, yDiff, texture, action_F);
 		this.imgPosX_F = imgPosX_F;
 		this.imgPosX_T = imgPosX_T;
@@ -35,6 +45,8 @@ public class PairedImageButtonSwitch extends ImageButton {
 		this.action_F = action_F;
 		this.action_T = action_T;
 		this.flag = flag;
+		hover_F = hoverText_F;
+		hover_T = hoverText_T;
 	}
 	
 	@Override
@@ -59,6 +71,10 @@ public class PairedImageButtonSwitch extends ImageButton {
 
 		blit(this.x, this.y, (float) (this.flag ? this.imgPosX_T : this.imgPosX_F), (float) i, this.width, this.height, 256, 256);
 		GlStateManager.enableDepthTest();
+	}
+	
+	public ITextComponent getTooltip() {
+		return flag ? hover_T : hover_F;
 	}
 	
 }
